d411b5e1d0304a5c4894a4b4d948ed38a1d958f2
Haitham <hmohamed.r1123@gmail.com>
2025-10-16 09:13:06 -0700

    MY SQL file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -16,7 +16,7 @@ CREATE TABLE Books ( book_id INT AUTO_INCREMENT PRIMARY KEY,
 
 CREATE TABLE Customers ( customer_id INT AUTO_INCREMENT PRIMARY KEY,
                         customer_name VARCHAR(215),
-                        email VARCHAR (215),
+                        email VARCHAR(215),
                         address TEXT
                     );
 
