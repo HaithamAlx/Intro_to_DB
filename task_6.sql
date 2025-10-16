94b6c188729b995b49daefda78f762045a3ca6ab
Haitham <hmohamed.r1123@gmail.com>
2025-10-16 13:09:51 -0700

    Task 6

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (3, 'Cole Baidoo', 'cbaidoo@sandtech.com', '125 Happiness Ave.');
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
